bdf2e861eb61f9904e574e03ebfe7d4e4c133f42
JustHNR <103069926+JustHNR@users.noreply.github.com>
2023-04-21 20:12:51 +0200

    API météo V1

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,45 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 const WEATHER_API_KEY = "10e295f3e3f3b5cdde7ea86fc5c6d5b2";
 
 function App() {
-  const [lat, setLat] = useState(10);
-  const [lon, setLon] = useState(40);
+  const [lat, setLat] = useState(undefined);
+  const [lon, setLon] = useState(undefined);
+
+  const [temp, setTemp] = useState(undefined);
+  const [ciel, setCiel] = useState(undefined);
+  const [icon, setIcon] = useState(undefined);
+
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.watchPosition(function (position) {
-        setLat(position.coords.latitude);
+        setLat(position.coords.latitude); 
         setLon(position.coords.longitude);
       });
     } else {
-      /* la géolocalisation n'est pas disponible */
+      setLat(48.858370);
+      setLon(2.294481); 
     }
   }, [])
 
 
   const getWeather = (lat, lon) => {
-    // votre code 
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=fr&units=metric`).then(
+      function(response) {
+        setCiel(response.data.weather[0].description);
+        setTemp(response.data.main.temp);
+        setIcon(response.data.weather[0].icon);
+      }
+    )
+    .catch(function (error) {
+      console.log(error);
+    })
   }
-  // le but de l'exercice est d'afficher la météo du jour en fonction des coordonées GPS récuperées au dessus. 
+  useEffect(() => {
+    if (lat && lon){
+      getWeather(lat, lon);
+    }
+  }, [lon, lat]);
+  // le but de l'exercice est d'afficher la météo du jour en fonction des coordonées GPS récuperées au dessus.
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>  API météo </h1>
       </header>
+      {temp && ciel && icon ? (
+        <div className="container">
+        <img alt="Icon du temps"src={`https://openweathermap.org/img/wn/${icon}@2x.png`} height="90px" width="90px"></img>
+        <p>{ciel}</p>
+        <p>{temp} °C</p>
+        </div>
+      ) : <p> Chargment...</p> }
+      
     </div>
   );
 }
